22bc02f3e0e4efb2d9e5be20bd248b7d9b5ee5f5
KingParchy <jparch@live.co.uk>
2024-02-03 11:51:32 +0900

    Changed the function `cleanInputString` to use a regular expression to remove all plus, minus, and whitespace characters from the input string. Added a new function `isInvalidInput` to check if the input string contains scientific notation. Modified the `addEntry` function to use the new `cleanInputString` and `isInvalidInput` functions.

--- Calorie_Counter/js/script.js
+++ Calorie_Counter/js/script.js
@@ -7,6 +7,22 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-    const strArray = str.split(""); // convert string to array of characters
-    const cleanStrArray = []; // create empty array for cleaned up version of input string
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");
+}
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(
+        `#${entryDropdown.value} .input-container`
+    );
+    const entryNumber =
+        targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>`;
 }
