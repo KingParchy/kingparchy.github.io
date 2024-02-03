8696f75b36952588722333c8dad8fb82fb7d642a
KingParchy <jparch@live.co.uk>
2024-02-03 16:12:04 +0900

    Add calorie calculation functionality and error handling.

--- Calorie_Counter/js/script.js
+++ Calorie_Counter/js/script.js
@@ -21,13 +21,61 @@ function addEntry() {
         `#${entryDropdown.value} .input-container`
     );
     const entryNumber =
-        targetInputContainer.querySelectorAll('input[type="text"]').length;
+        targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
     <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"></input>`;
-    targetInputContainer.innerHTML += HTMLString;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+
+    const breakfastNumberInputs = document.querySelectorAll(
+        `#breakfast input[type=number]`
+    );
+    const lunchNumberInputs = document.querySelectorAll(
+        `#lunch input[type=number]`
+    );
+    const dinnerNumberInputs = document.querySelectorAll(
+        `#dinner input[type=number]`
+    );
+    const snacksNumberInputs = document.querySelectorAll(
+        `#snacks input[type=number]`
+    );
+    const exerciseNumberInputs = document.querySelectorAll(
+        `#exercise input[type=number]`
+    );
+
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+    if (isError) {
+        return;
+    }
+}
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+
+    for (let i = 0; i < list.length; i++) {
+        const currVal = cleanInputString(list[i].value);
+        const invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
+    }
+    return calories;
 }
 
 addEntryButton.addEventListener("click", addEntry);
